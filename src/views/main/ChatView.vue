c5d82cb8bce8ba104dd4dbe098046ea4bb710361
zhuzhaofeng <zhuzhaofeng@aliyun.com>
2023-05-09 23:56:22 +0800

    🐛fix(error): 错误显示修复

--- src/views/main/ChatView.vue
+++ src/views/main/ChatView.vue
@@ -384,10 +384,12 @@ const placeholder = computed(() => {
 
       &.is-assistant {
         @apply bg-white dark:bg-dark;
-        // &.is-error {
-        //   background-color: rgb(var(--red-1)) !important;
-        //   color: rgb(var(--red-6)) !important;
-        // }
+        &.is-error {
+          // background-color: rgb(var(--red-1)) !important;
+          .markdown-body {
+            color: rgb(var(--red-6)) !important;
+          }
+        }
       }
     }
 
